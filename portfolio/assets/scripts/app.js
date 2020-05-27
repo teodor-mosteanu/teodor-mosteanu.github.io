1c72de9949690412f44a4d6711aa475eae8e186f
Teodor Mosteanu <teodor.mosteanu@gmail.com>
2020-05-27 14:24:06 +0100

    clean portfolio holder

---
+++ portfolio/assets/scripts/app.js
@@ -0,0 +1,108 @@
+const Portfolio = function() {
+	function makeWords() {
+		var words = [
+			{
+				text: "es6",
+				weight: 12.3
+			}, {
+				text: "css3",
+				weight: 8
+			}, {
+				text: "javascript",
+				weight: 14
+			}, {
+				text: "node",
+				weight: 3
+			}, {
+				text: "programming",
+				weight: 7
+			}, {
+				text: "python",
+				weight: 10
+			}, {
+				text: "java",
+				weight: 9
+			}, {
+				text: "react",
+				weight: 15
+			}, {
+				text: "newCode",
+				weight: 7
+			}
+		];
+		return words;
+	}
+
+	function makeWordCloud(words) {
+		$('.teaching-domains').jQCloud(words, {delay: 120});
+	}
+
+	function displayWordCloud() {
+		var count = 1;
+		$(window).on('scroll', function() {
+			var y_scroll_pos = window.pageYOffset;
+			var scroll_pos_test = 2700; // set to whatever you want it to be
+			var words = makeWords();
+			if (y_scroll_pos > scroll_pos_test && count <= 1) {
+				makeWordCloud(words);
+				count++;
+			}
+		});
+	}
+
+	function designForm() {
+		$("#my-modal form").addClass("my-form");
+	}
+
+	function typeAnimation() {
+		Typed.new("#writing-text", {
+			strings: [
+				"am a Junior Web Developer.", "love everything about code.", "solve problems."
+			],
+			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+			stringsElement: null,
+			// typing speed
+			typeSpeed: 1,
+			contentType: 'text',
+			callback: function() {
+				$("#writing-text").css({"color": "#fff", "background-color": "#C8412B"});
+			},
+			preStringTyped: function() {},
+			onStringTyped: function() {}
+		});
+	}
+
+	const { styler, spring, listen, pointer, value } = window.popmotion;
+
+const ball = document.querySelector('.box');
+const divStyler = styler(ball);
+const ballXY = value({ x: 0, y: 0 }, divStyler.set);
+
+listen(ball, 'mousedown touchstart')
+  .start((e) => {
+    e.preventDefault();
+    pointer(ballXY.get()).start(ballXY);
+  });
+
+listen(document, 'mouseup touchend')
+  .start(() => {
+    spring({
+      from: ballXY.get(),
+      velocity: ballXY.getVelocity(),
+      to: { x: 0, y: 0 },
+      stiffness: 200,
+      // mass: 1,
+      // damping: 10
+    }).start(ballXY);
+  });
+
+	return {
+		displayWordCloud: displayWordCloud,
+		typeAnimation: typeAnimation
+	}
+
+}();
+
+
+Portfolio.displayWordCloud();
+Portfolio.typeAnimation();
