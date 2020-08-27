409b6fd3b42d0cbe85d8ffa29a270ecbef8a73ba
Teodor Mosteanu <teodor.mosteanu@gmail.com>
2020-08-27 15:57:03 +0100

    Final
    Final with pre-live

--- JAM/web/js/scripts.js
+++ JAM/web/js/scripts.js
@@ -23,13 +23,13 @@
 	
 	/* Navbar Scripts */
 	// jQuery to collapse the navbar on scroll
-    $(window).on('scroll load', function() {
-		if ($(".navbar").offset().top > 20) {
-			$(".fixed-top").addClass("top-nav-collapse");
-		} else {
-			$(".fixed-top").removeClass("top-nav-collapse");
-		}
-    });
+    // $(window).on('scroll load', function() {
+	// 	if ($(".navbar").offset().top > 20) {
+	// 		$(".fixed-top").addClass("top-nav-collapse");
+	// 	} else {
+	// 		$(".fixed-top").removeClass("top-nav-collapse");
+	// 	}
+    // });
 
 	// jQuery for page scrolling feature - requires jQuery Easing plugin
 	$(function() {
@@ -195,38 +195,6 @@
 		removalDelay: 300,
 		mainClass: 'my-mfp-slide-bottom'
 	});
-    
-
-    /* Counter - CountTo */
-	var a = 0;
-	$(window).scroll(function() {
-		if ($('#counter').length) { // checking if CountTo section exists in the page, if not it will not run the script and avoid errors	
-			var oTop = $('#counter').offset().top - window.innerHeight;
-			if (a == 0 && $(window).scrollTop() > oTop) {
-			$('.counter-value').each(function() {
-				var $this = $(this),
-				countTo = $this.attr('data-count');
-				$({
-				countNum: $this.text()
-				}).animate({
-					countNum: countTo
-				},
-				{
-					duration: 2000,
-					easing: 'swing',
-					step: function() {
-					$this.text(Math.floor(this.countNum));
-					},
-					complete: function() {
-					$this.text(this.countNum);
-					//alert('finished');
-					}
-				});
-			});
-			a = 1;
-			}
-		}
-    });
 
 
     /* Move Form Fields Label When User Types */
@@ -241,131 +209,9 @@
 
 
     /* Contact Form */
-    $("#contactForm").validator().on("submit", function(event) {
-    	if (event.isDefaultPrevented()) {
-            // handle the invalid form...
-            cformError();
-            csubmitMSG(false, "Please fill all fields!");
-        } else {
-            // everything looks good!
-            event.preventDefault();
-            csubmitForm();
-        }
-    });
-
-    function csubmitForm() {
-        // initiate variables with form content
-		var name = $("#cname").val();
-		var email = $("#cemail").val();
-        var message = $("#cmessage").val();
-        var terms = $("#cterms").val();
-        $.ajax({
-            type: "POST",
-            url: "php/contactform-process.php",
-            data: "name=" + name + "&email=" + email + "&message=" + message + "&terms=" + terms, 
-            success: function(text) {
-                if (text == "success") {
-                    cformSuccess();
-                } else {
-                    cformError();
-                    csubmitMSG(false, text);
-                }
-            }
-        });
-	}
-
-    function cformSuccess() {
-        $("#contactForm")[0].reset();
-        csubmitMSG(true, "Message Submitted!");
-        $("input").removeClass('notEmpty'); // resets the field label after submission
-        $("textarea").removeClass('notEmpty'); // resets the field label after submission
-    }
-
-    function cformError() {
-        $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-            $(this).removeClass();
-        });
-	}
-
-    function csubmitMSG(valid, msg) {
-        if (valid) {
-            var msgClasses = "h3 text-center tada animated";
-        } else {
-            var msgClasses = "h3 text-center";
-        }
-        $("#cmsgSubmit").removeClass().addClass(msgClasses).text(msg);
-    }
-
-
-    /* Privacy Form */
-    $("#privacyForm").validator().on("submit", function(event) {
-    	if (event.isDefaultPrevented()) {
-            // handle the invalid form...
-            pformError();
-            psubmitMSG(false, "Please fill all fields!");
-        } else {
-            // everything looks good!
-            event.preventDefault();
-            psubmitForm();
-        }
-    });
-
-    function psubmitForm() {
-        // initiate variables with form content
-		var name = $("#pname").val();
-		var email = $("#pemail").val();
-        var select = $("#pselect").val();
-        var terms = $("#pterms").val();
-        
-        $.ajax({
-            type: "POST",
-            url: "php/privacyform-process.php",
-            data: "name=" + name + "&email=" + email + "&select=" + select + "&terms=" + terms, 
-            success: function(text) {
-                if (text == "success") {
-                    pformSuccess();
-                } else {
-                    pformError();
-                    psubmitMSG(false, text);
-                }
-            }
-        });
-	}
-
-    function pformSuccess() {
-        $("#privacyForm")[0].reset();
-        psubmitMSG(true, "Request Submitted!");
-        $("input").removeClass('notEmpty'); // resets the field label after submission
-    }
-
-    function pformError() {
-        $("#privacyForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-            $(this).removeClass();
-        });
-	}
-
-    function psubmitMSG(valid, msg) {
-        if (valid) {
-            var msgClasses = "h3 text-center tada animated";
-        } else {
-            var msgClasses = "h3 text-center";
-        }
-        $("#pmsgSubmit").removeClass().addClass(msgClasses).text(msg);
-    }
     
 
-    /* Back To Top Button */
-    // create the back to top button
-    $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
-    var amountScrolled = 700;
-    $(window).scroll(function() {
-        if ($(window).scrollTop() > amountScrolled) {
-            $('a.back-to-top').fadeIn('500');
-        } else {
-            $('a.back-to-top').fadeOut('500');
-        }
-    });
-
+    
 
 	/* Removes Long Focus On Buttons */
 	$(".button, a, button").mouseup(function() {
@@ -373,3 +219,72 @@
 	});
 
 })(jQuery);
+
+// vanilla JS
+
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 25px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 20) {
+    mybutton.style.visibility = "visible";
+    mybutton.style.opacity = "1";
+  } else {
+    mybutton.style.visibility = "hidden";
+    mybutton.style.opacity = "0";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+let tab_3=document.getElementById("tab-3");
+let tab_2=document.getElementById("tab-2");
+let tab_1=document.getElementById("tab-1");
+let tab_menu3=document.getElementById("nav-tab-3");
+let tab_menu2=document.getElementById("nav-tab-2");
+let tab_menu1=document.getElementById("nav-tab-1");
+
+document.getElementById("schedule").addEventListener("click", function(){
+    tab_menu1.setAttribute("aria-selected", false);
+    tab_menu1.classList.remove("active");
+    tab_menu2.setAttribute("aria-selected", "true");
+    tab_menu2.classList.add("active");
+    tab_menu3.setAttribute("aria-selected", false);
+    tab_menu3.classList.remove("active");
+    tab_1.classList.remove("active", "show");
+    tab_2.classList.add("active", "show");
+})
+document.getElementById("about-jam").addEventListener("click", function(){
+    tab_menu2.setAttribute("aria-selected", false);
+    tab_menu2.classList.remove("active");
+    tab_menu1.setAttribute("aria-selected", "true");
+    tab_menu1.classList.add("active");
+    tab_menu3.setAttribute("aria-selected", false);
+    tab_menu3.classList.remove("active");
+    tab_2.classList.remove("active", "show");
+    tab_1.classList.add("active", "show");
+    
+})
+document.getElementById("register-jam").addEventListener("click", function(){
+    tab_menu1.setAttribute("aria-selected", false);
+    tab_menu2.setAttribute("aria-selected", false);
+    tab_menu1.classList.remove("active");
+    tab_menu2.classList.remove("active");
+    tab_menu3.setAttribute("aria-selected", "true");
+    tab_menu3.classList.add("active");
+    tab_1.classList.remove("active", "show");
+    tab_2.classList.remove("active", "show");
+    tab_3.classList.add("active", "show");
+    console.log("true")
+})
+
+
+
+
+
